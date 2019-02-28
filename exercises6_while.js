0856ef8a886680e20b00658474047d77fb4f7fa0
tanyadanang <47728820+tanyadanang@users.noreply.github.com>
2019-03-01 04:06:44 +0700

    Add files via upload

---
+++ exercises6_while.js
@@ -0,0 +1,35 @@
+//1. Melakukan Looping Menggunakan While
+console.log('1. Melakukan Looping Menggunakan While')
+var deret=0
+
+console.log ('LOOPING PERTAMA')
+while (deret <20){
+    deret+=2
+    console.log (deret+ ' - I love coding')
+}
+
+console.log ('LOOPING KEDUA')
+
+var deret=22
+
+while (deret >2){
+    deret-=2
+    console.log (deret+ ' - I will become fullstack developer')
+}
+
+//2. Melakukan Looping Menggunakan For
+
+console.log(' ')
+console.log('2. Melakukan Looping Menggunakan For')
+console.log ('LOOPING PERTAMA')
+for (var deret=1; deret<21;deret+=1){
+    console.log(deret+' - I love coding')
+}
+console.log ('LOOPING KEDUA')
+for (var deret=20; deret>0; deret -=1){
+    console.log (deret+ ' - I will become fullstack developer')
+}
+
+//3. Angka Ganjil dan Genap
+console.log ('')
+console.log ('3. Angka Ganjil dan Genap')
