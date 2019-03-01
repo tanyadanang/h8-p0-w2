55f2b61e07d328f05e57be019626058a7d2e7a45
tanyadanang <47728820+tanyadanang@users.noreply.github.com>
2019-03-01 14:00:28 +0700

    Update exercises5_string.js

--- exercises5_string.js
+++ exercises5_string.js
@@ -27,6 +27,14 @@ console.log('Third Word: ' + thirdWord);
 console.log('Fourth Word: ' + fourthWord);
 console.log('Fifth Word: ' + fifthWord);
 
+//Result
+/*
+First Word: wow
+Second Word: JavaScript
+Third Word: is
+Fourth Word: so
+Fifth Word: cool
+*/ 
 console.log('')
 //3. Breaking Sentence (Again) using Substring
 console.log('3. Breaking Sentence (Again) using Substring')
@@ -42,3 +50,45 @@ console.log('Second Word: ' + secondWord);
 console.log('Third Word: ' + thirdWord);
 console.log('Fourth Word: ' + fourthWord);
 console.log('Fifth Word: ' + fifthWord);
+
+//Result
+/*
+First Word: wow
+Second Word: JavaScript
+Third Word: is
+Fourth Word: so
+Fifth Word: cool
+*/
+console.log('')
+//4. Breaking Sentence (yet Again) and Count Each Length
+console.log ('4. Breaking Sentence (yet Again) and Count Each Length')
+
+var word4 = 'wow JavaScript is so cool';
+var exampleFirstWord4 = word.substring(0, 3);
+var secondWord4 = word.substring(4,14); // do your own!
+var thirdWord4 = word.substring(17,15); // do your own!
+var fourthWord4 = word.substring(20,18); // do your own!
+var fifthWord4 = word.substring(25,21); // do your own!
+
+var firstWordLength = exampleFirstWord4.length;
+var secondWord4Length = secondWord4.length;
+var thirdWord4Length = thirdWord4.length;
+var fourthWord4Length = fourthWord4.length
+var fifthWord4Length = fifthWord4.length;
+// create new variables around here
+
+console.log('First Word: ' + exampleFirstWord + ', with length: ' + firstWordLength);
+console.log('Second Word: ' + secondWord4+', with length: '+secondWord4Length);
+console.log('Third Word: ' + thirdWord4+', with length: '+thirdWord4Length);
+console.log('Fourth Word: ' + fourthWord4+', with length: '+fourthWord4Length);
+console.log('Fifth Word: ' + fifthWord4+', with length: '+fifthWord4Length);
+
+
+//Result
+/*
+First Word: wow, with length: 3
+Second Word: JavaScript, with length: 10
+Third Word: is, with length: 2
+Fourth Word: so, with length: 2
+Fifth Word: cool, with length: 4
+*/
