746c85c4511364128b71731509fd93452476c6ac
tanyadanang <47728820+tanyadanang@users.noreply.github.com>
2019-03-04 02:18:55 +0700

    Update exercises13_x_dan_o.js

--- exercises13_x_dan_o.js
+++ exercises13_x_dan_o.js
@@ -1,8 +1,21 @@
-//Exercises X and O
-
 function xo(str) {
     
-    
+    var hurufX = 0; 
+    var hurufO = 0; 
+    for(i=0; i<str.length; i++){
+        if(str[i] === 'x'){
+            hurufX++;
+        }
+        else if(str[i] === 'o'){
+            hurufO++;
+        }
+    }
+    if(hurufX === hurufO){
+        return true;
+    }
+    else{
+        return false;
+    }
   }
   
   // TEST CASES
