a2fe3934f1f8bd7dae26ddbb77714f0bfde24e15
tanyadanang <47728820+tanyadanang@users.noreply.github.com>
2019-03-03 20:33:35 +0700

    Update exercises12_konversi_menit.js

--- exercises12_konversi_menit.js
+++ exercises12_konversi_menit.js
@@ -1,16 +1,21 @@
 //Exercises Konversi Menit
 
-//Exercises Konversi Menit
+
 
 function konversiMenit(menit) {
-    // code
-
+    var sisamenit = menit%60
+    var jam = (menit - sisamenit) / 60
+    console.log(jam + ":" + sisamenit) 
     
-  }
-  
-  // TEST CASES
+}
+
+
+
   console.log(konversiMenit(63)); // 1:03
   console.log(konversiMenit(124)); // 2:04
   console.log(konversiMenit(53)); // 0:53
   console.log(konversiMenit(88)); // 1:28
   console.log(konversiMenit(120)); // 2:00
+
+
+
