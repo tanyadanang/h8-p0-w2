bdbe4f315fae04c7c7972ebe5311336bff84cdef
tanyadanang <47728820+tanyadanang@users.noreply.github.com>
2019-03-03 13:08:15 +0700

    Update exercises12_konversi_menit.js

--- exercises12_konversi_menit.js
+++ exercises12_konversi_menit.js
@@ -1,1 +1,16 @@
 //Exercises Konversi Menit
+
+//Exercises Konversi Menit
+
+function konversiMenit(menit) {
+    // code
+
+    
+  }
+  
+  // TEST CASES
+  console.log(konversiMenit(63)); // 1:03
+  console.log(konversiMenit(124)); // 2:04
+  console.log(konversiMenit(53)); // 0:53
+  console.log(konversiMenit(88)); // 1:28
+  console.log(konversiMenit(120)); // 2:00
