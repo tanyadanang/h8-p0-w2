7edff31704f09b133c6c126e40268dfeeaae86c0
tanyadanang <47728820+tanyadanang@users.noreply.github.com>
2019-03-03 13:08:42 +0700

    Update exercises13_x_dan_o.js

--- exercises13_x_dan_o.js
+++ exercises13_x_dan_o.js
@@ -1,1 +1,14 @@
 //Exercises X and O
+
+//Exercises X and O
+
+function xo(str) {
+    
+  }
+  
+  // TEST CASES
+  console.log(xo('xoxoxo')); // true
+  console.log(xo('oxooxo')); // false
+  console.log(xo('oxo')); // false
+  console.log(xo('xxxooo')); // true
+  console.log(xo('xoxooxxo')); // true
